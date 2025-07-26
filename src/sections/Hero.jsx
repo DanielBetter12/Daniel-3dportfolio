1b972fd166748f00732efa470a8989bbb8ddea36
Daniel <dbetter66@gmail.com>
2025-07-25 20:31:49 -0600

    almost done with hero page

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import {words} from "../constants/index.js";
-
+import Button from "../components/Button.jsx";
+import HeroExperience from "../components/HeroModels/HeroExperience.jsx";
 
 
 const Hero = () => {
@@ -16,27 +17,42 @@ const Hero = () => {
                     <div className="flex flex-col gap-7">
                         <div className="hero-text">
                             <h1>Shaping
-                              <span className="slide">
+                                <span className="slide">
                                   <span className="wrapper">
-                                      {words.map((word) =>(
-                                          <span key={word.text} className="flex items-center md:gap-3 gap-1 pb-2">
+                                      {words.map((word, index) => (
+                                          <span key={index} className="flex items-center md:gap-3 gap-1 pb-2">
                                             <img
-                                            src={word.imgPath}
-                                             alt={word.text}
-                                            className="xl:size-12 md:size-10 size-7 md:p-2 p-1 rounded-full bg-white-50"
+                                                src={word.imgPath}
+                                                alt="person"
+                                                className="xl:size-12 md:size-10 size-7 md:p-2 p-1 rounded-full bg-white-50"
                                             />
                                           <span>{word.text}</span>
                                   </span>
-                                          ))}
+                                      ))}
                                   </span>
                               </span>
                             </h1>
+
                             <h1>into Real Projects</h1>
                             <h1>that Deliver Results</h1>
                         </div>
+                        <p className="text-white-50 md:text-xl relative z-10 pointer-events-none">
+                            Hi, I'm Daniel, a developer based in Denver, Colorado with a passion for code.
+                        </p>
+                        <Button
+                        className="md:w-80 md:h-16 w-60 h-12"
+                        id="button"
+                        text="See my Work"
+                        />
                     </div>
-
                 </header>
+
+                {/*Right: 3d model */}
+                <figure>
+                   <div className="hero-3d-layout">
+                    <HeroExperience />
+                   </div>
+                </figure>
             </div>
         </section>
     );
